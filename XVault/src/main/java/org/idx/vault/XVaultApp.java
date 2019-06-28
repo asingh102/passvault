6060ae5dcbbcf520d4b09ded889b4c78e5b17695
Anurag Singh <anurag.singh@idx-one.com>
2019-06-28 22:48:45 +0530

    Initial commit

---
+++ XVault/src/main/java/org/idx/vault/XVaultApp.java
@@ -0,0 +1,18 @@
+package org.idx.vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author 
+ *
+ */
+@SpringBootApplication
+public class XVaultApp 
+{
+    public static void main( String[] args )
+    {
+	    SpringApplication.run(XVaultApp.class, args);
+    }
+}
---
+++ XVault/src/test/java/org/idx/vault/XVaultAppTest.java
@@ -0,0 +1,38 @@
+package org.idx.vault;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class XVaultAppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public XVaultAppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( XVaultAppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
